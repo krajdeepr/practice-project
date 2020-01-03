31b6afe8abb53aecfd74131350c2c9f8d0bd05c4
Rajdeep <rajdeep_katamreddy@epam.com>
2020-01-03 12:42:47 +0530

    code cleaned

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -1,78 +1,40 @@
 import data from "../data";
 import { Component, OnInit, Input } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms"; 
-import { config } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
 @Component({
-  selector: 'app-dynamic-form',
-  templateUrl: './dynamic-form.component.html',
-  styleUrls: ['./dynamic-form.component.css']
+  selector: "app-dynamic-form",
+  templateUrl: "./dynamic-form.component.html",
+  styleUrls: ["./dynamic-form.component.css"]
 })
 export class DynamicFormComponent implements OnInit {
-  formData:any={};
-  form;
-  formObj;
-  param_key = [];
+  formData: any = {};
+  form: any = {};
+  formObj: any = [];
   param_value = [];
   submitted: boolean;
-  dataId;
+  dataId: number;
   numParams: number;
   paramName: string;
   constructor(private route: ActivatedRoute) {}
-   formGroup = {};
+  formGroup = {};
   ngOnInit() {
-    
-    this.formData=data.dataroot.Functions;
-   
-    // console.log(this.form);
-    this.dataId = +this.route.snapshot.paramMap.get('index');
-    // console.log(this.dataId);
-    let id = this.dataId;
-    this.formObj=this.formData.filter(function(val) {
-      return val.Id==id;
+    this.formData = data.dataroot.Functions;
+    this.dataId = +this.route.snapshot.paramMap.get("index");
+    this.formObj = this.formData.filter(val => {
+      return val.Id == this.dataId;
     });
-    for(let i=0;i<this.formObj[0].NumParms;i++){
-      let par = `Parm${i+1}_Name`;
-     
-      // this.param_name.push(
-      //   {
-      //     [`Parm${i+1}_Name`]:this.formObj[0][par]
-      //   });
-      this.param_key.push(par);
-      this.param_value.push(this.formObj[0][par])
+    for (let i = 0; i < this.formObj[0].NumParms; i++) {
+      this.param_value.push(this.formObj[0][`Parm${i + 1}_Name`]);
     }
-       this.param_value.forEach(formControl => {
-        this.formGroup[formControl] = new FormControl("");
-    })
+    this.param_value.forEach(formControl => {
+      this.formGroup[formControl] = new FormControl("");
+    });
     this.form = new FormGroup(this.formGroup);
-    console.log(this.form);
-    
   }
 
   submitForm() {
     this.submitted = true;
     console.log(this.form);
   }
-
-  // public getInputName(inputNumber: number): string {
-  //   // console.log(this.formData[this.dataIndex]);
-  //   // console.log(inputNumber);
-  //   this.paramName=this.formData[this.dataIndex][`Parm${inputNumber+1}_Name`];
-  //   return this.paramName;
-  // }
-  // getInputBoxName(inputNumber:number){
-    
-  //   this.formData.forEach(formControl => {
-  //     for(let i=0;i<formControl.NumParms;i++){
-  //       let formName = `Parm${i}_Name`;
-  //       this.form.patchValue({
-  //         formName:''
-  //       });
-  //       // this.formGroup[formControl.formName] = new FormControl("");
-  //     }
-      
-  //   });
-  //   console.log(`Parm${inputNumber+1}_Name`);
-  //   return `Parm${inputNumber+1}_Name`;
-  // }
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,41 +1,40 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatSort} from '@angular/material';
-import {MatTableDataSource} from '@angular/material/table';
-import data from '../data'
-import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatSort } from "@angular/material";
+import { MatTableDataSource } from "@angular/material/table";
+import data from "../data";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  selector: "app-table",
+  templateUrl: "./table.component.html",
+  styleUrls: ["./table.component.css"]
 })
 export class TableComponent implements OnInit {
-  displayedColumns: string[] = ['Id','Name', 'Mnemonic'];
+  displayedColumns: string[] = ["Id", "Name", "Mnemonic"];
   dataSource = new MatTableDataSource(data.dataroot.Functions);
   selectedRowIndex: any;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
-constructor(private router: Router){}
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  constructor(private router: Router) {}
   ngOnInit() {
     this.dataSource.sort = this.sort;
   }
-  dblclick(index){
-    this.router.navigate(['/dynamicForm', {index}]);
+  dblclick(index) {
+    this.router.navigate(["/dynamicForm", { index }]);
   }
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  highlight(index: number){
-    if(index >= 0 && index <= data.dataroot.Functions.length - 1)
-       this.selectedRowIndex = index;
-   //console.log(this.selectedRowIndex);
- }
+  highlight(index: number) {
+    if (index >= 0 && index <= data.dataroot.Functions.length - 1)
+      this.selectedRowIndex = index;
+  }
 
-  arrowUpEvent(index: number){
-   this.highlight(index - 1);
- }
+  arrowUpEvent(index: number) {
+    this.highlight(index - 1);
+  }
 
- arrowDownEvent(index: number){
-   console.log(index);
+  arrowDownEvent(index: number) {
+    console.log(index);
     this.highlight(index + 1);
- }
+  }
 }
